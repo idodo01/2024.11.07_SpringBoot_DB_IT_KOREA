18276ecec2d853fd217fe7420e5121dd08211935
IDO <susususu07@naver.com>
2024-11-07 19:21:12 +0900

    Add files via upload

---
+++ src/main/java/com/koreait/db/SpringDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/db/SpringDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
